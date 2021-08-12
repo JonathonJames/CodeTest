ca23d071a477ddede056f4c16143b159dd16cd8c
Jonathon James <jonathonjames@gmail.com>
2021-08-12 09:54:20 +0100

    Add BookmarkedListing managed object

---
+++ Job Search/Job Search/API/Model/BookmarkedListing+CoreDataClass.swift
@@ -0,0 +1,25 @@
+//
+//  BookmarkedListing+CoreDataClass.swift
+//  Job Search
+//
+//  Created by Uncreation on 12/08/2021.
+//
+//
+
+import Foundation
+import CoreData
+
+/// Describes a job listing which has been bookmarked.
+@objc(BookmarkedListing)
+public class BookmarkedListing: NSManagedObject {
+    
+    @NSManaged public var id: Int64
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<BookmarkedListing> {
+        return NSFetchRequest<BookmarkedListing>(entityName: "BookmarkedListing")
+    }
+}
+
+// MARK: Identifiable conformance
+
+extension BookmarkedListing : Identifiable {}
